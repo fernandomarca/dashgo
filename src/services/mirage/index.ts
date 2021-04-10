bcd78aafb7c874487bd628bd669651473446ab98
Fernando Marca Magalhães <59258828+fernandomarca@users.noreply.github.com>
2021-04-10 19:52:27 -0300

    react-query prefetch

--- src/services/mirage/index.ts
+++ src/services/mirage/index.ts
@@ -1,13 +1,21 @@
-import { createServer, Factory, Model } from "miragejs";
 import faker from "faker";
+import { createServer, Factory, Model, Response } from "miragejs";
 type User = {
   name: string;
   email: string;
   created_at: string;
 };
 
+//User
+//address
+
+//address (id)
+//{name:"Diego", email:"...."}
+
 export function makeServer() {
   const server = createServer({
+    serializers: {},
+
     models: {
       user: Model.extend<Partial<User>>({}),
     },
@@ -27,13 +35,32 @@ export function makeServer() {
     },
 
     seeds(server) {
-      server.createList("user", 15);
+      server.createList("user", 200);
     },
 
     routes() {
       this.namespace = "api";
       this.timing = 750;
-      this.get("/users");
+      this.get("/users", function (schema, request) {
+        const { page = 1, per_page = 10 } = request.queryParams;
+
+        const total = schema.all("user").length;
+
+        //page 2 ir do 10 - 20
+        const pageStart = (Number(page) - 1) * Number(per_page);
+        // ex: page=3
+        // pageStart = 2 * 10 = 20
+        const pageEnd = pageStart + Number(per_page);
+        //pageEnd = 20 + 10 = 30
+
+        const users = this.serialize(schema.all("user")).users.slice(
+          pageStart,
+          pageEnd
+        );
+
+        return new Response(200, { "x-total-count": String(total) }, { users });
+      });
+      this.get("/users/:id");
       this.post("/users");
 
       this.namespace = "";
